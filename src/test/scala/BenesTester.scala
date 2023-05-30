cf42f304356ae283b021c0844bbca2f27a2d7dcc
shahzaibk23 <shahzaibceo@gmail.com>
2023-05-30 18:36:10 +0500

    add test case

---
+++ src/test/scala/BenesTester.scala
@@ -0,0 +1,42 @@
+import chisel3._
+import chisel3.util._
+import chiseltest._ 
+import org.scalatest.freespec.AnyFreeSpec
+
+class BenesTester extends AnyFreeSpec with ChiselScalatestTester {
+    "Simple Benes Test" in {
+        val DATA_TYPE = 16
+        val NUM_PES = 8
+        val LEVELS = 7
+        test(new Benes(DATA_TYPE, NUM_PES, LEVELS)).withAnnotations(Seq(VerilatorBackendAnnotation)) { dut =>
+
+            
+            val NUM_TESTS = 3
+            val i_mux_bus = List(
+                BigInt("FFFFFFFFFF", 16),
+                BigInt("0000000000", 16),
+                BigInt("FF00000000", 16)
+            )
+            val i_data_bus = BigInt("77776666555544443333222211110000", 16)
+            
+            
+            var counter = 0
+            for (t <- 0 until NUM_TESTS) {
+                println(s"counter: $counter")
+
+                (0 until 8).map(i => dut.io.i_data_bus(i).poke((((i_data_bus >> (16 * i)) & 0xFFFF).toInt).U))
+                String.format(s"%0${2 * (LEVELS - 2) * NUM_PES + NUM_PES}d", counter.toBinaryString.toInt).replace(' ', '0').zipWithIndex.map{ case (c,i) => if( c=='1' ) dut.io.i_mux_bus(i).poke(true.B) else dut.io.i_mux_bus(i).poke(false.B) }
+                
+                if(counter < NUM_TESTS-1) {
+                    counter += 1
+                } else {
+                    counter = 0
+                }
+                dut.clock.step(1)
+            }
+
+        }
+    }
+
+  
+}
